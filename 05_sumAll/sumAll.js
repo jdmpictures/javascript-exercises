277a02b5e47c3ec46f558521f9403ced97da3d4c
Jack Marles <jdmpictures@gmail.com>
2022-10-27 22:43:14 +0100

    SumAll completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let sum = 0; 
+    if(typeof min !== 'number' || typeof max !== 'number'){
+        return 'ERROR'
+    } else if (min < 0 || max < 0) {
+        return 'ERROR'
+    }  else if (min < max) { 
+        for(i = min; i <= max; i++) {
+            sum += i;
+        }
+    } else {
+        for (i = max; i <= min; i++){
+            sum += i;
+        }
+
+    }
+
+    return sum;
+
 
 };
 
