ecb6617cd3571400cbd5526b5a7142b6b4815dd2
Jack Marles <jdmpictures@gmail.com>
2022-10-31 23:43:49 +0000

    Calculator task completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,46 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+  let accumulator = 0
+  for(i = 0; i < arr.length; i++){
+    accumulator += arr[i];
+  }
+  return accumulator; 
+
 	
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let accumulator = 1;
+  for(i = 0; i < arr.length; i++){
+    accumulator *= arr[i] 
+  }
+  return accumulator;
 
 };
 
-const power = function() {
+const power = function(a, b) {
+  return Math.pow(a, b);
 	
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  if (num === 0 || num === 1)
+  return 1;
+for(i = num - 1; i >= 1; i--){
+  num *= i;
+} 
+
+return num
+
 	
 };
 
