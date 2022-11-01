f2214e42d5ca6eb6e43ebe9997ab651b2cabab4f
Jack Marles <jdmpictures@gmail.com>
2022-11-01 11:38:51 +0000

    Palindrome challenge completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,23 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let  reversedStr = str
+    .split("")
+    .reverse()
+    .join("")
+    .replace(/[\W\s]/g, "")
+    .toLowerCase()
+
+    let temp = str.replace(/[\W\s]/g, "")
+    .toLowerCase();
+
+    console.log(temp)
+    console.log(reversedStr)
+
+    if(temp === reversedStr){
+        return true;
+    } else{
+        return false;
+    }
+    
 
 };
 
