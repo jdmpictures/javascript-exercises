01faa8750a82bfbcaf1c38a323ef8d1440e25e72
Jack Marles <jdmpictures@gmail.com>
2022-11-02 00:08:58 +0000

    Finished fibonacci challenge

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let fib = [0, 1];
+    if(num < 0){
+        return 'OOPS'
+    }
+
+    for(i = 2; i <= num; i++){
+        fib[i] = fib[i - 2] + fib[i - 1]
+        fib.push(fib[i])
+    }
+    return fib[fib.length - 1]
 
 };
 
